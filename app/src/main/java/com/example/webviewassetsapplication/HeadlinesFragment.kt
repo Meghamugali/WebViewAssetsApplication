53077a17e7182bc3e2d2de2da67ec64384bf42d1
Megha <meghabmugali@gmail.com>
2022-10-03 15:27:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webviewassetsapplication/HeadlinesFragment.kt
@@ -0,0 +1,42 @@
+package com.example.webviewassetsapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class HeadlinesFragment: Fragment(), AdapterView.OnItemClickListener {
+    lateinit var headlinesListView: ListView
+
+    lateinit var headlinesClickListener : HeadlinesClickListener
+
+    interface HeadlinesClickListener{               //switchboard
+        fun onHeadlineClicked(headline:String)      //switch
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        headlinesClickListener = activity as HeadlinesClickListener //wiring
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+         super.onCreateView(inflater, container, savedInstanceState)
+        var view = inflater.inflate(R.layout.fragment_headlines,container,false)
+        headlinesListView = view.findViewById(R.id.headlinesListview)
+        headlinesListView.setOnItemClickListener(this)
+        return view
+    }
+
+    override fun onItemClick(adapterview: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        var itemClicked : String = adapterview?.getItemAtPosition(position).toString()
+        headlinesClickListener.onHeadlineClicked(itemClicked)
+    }
+}
---
+++ app/src/main/java/com/example/webviewassetsapplication/NewsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.webviewassetsapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class NewsActivity : AppCompatActivity(), HeadlinesFragment.HeadlinesClickListener{  //appliance
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+
+    override fun onHeadlineClicked(headline: String) {
+        Toast.makeText(this,headline, Toast.LENGTH_SHORT).show()
+        var newsContentFragment = supportFragmentManager.findFragmentById(R.id.contentFrag) as NewsContentFragment
+        newsContentFragment.setHeadline(headline)
+
+    }
+}
